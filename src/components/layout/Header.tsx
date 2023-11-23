5053406297ea81049577642bfb202f23747ea6e7
Ch-Saqib <125819263+Ch-Saqib@users.noreply.github.com>
2023-11-23 12:33:44 +0500

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="flex justify-center mt-7">
+    <div className="flex justify-center mt-7 px-4 md:px-6 lg:px-6">
       <div className="flex  justify-center w-[400px] h-[30px] border bg-gray-400 text-lg font-bold">
         Contact List
       </div>
