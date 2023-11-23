2890d2396e3ba1fa58d0560ccc7eaa9b9c3a3a0c
Ch-Saqib <125819263+Ch-Saqib@users.noreply.github.com>
2023-11-23 12:34:07 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-
 import AllContact from "./contact/page";
 
 export default function Home() {
   return (
-    <div>
-    <div className="flex  ml-[695px] overflow-y-auto bg-gray-300  w-[400px] h-[400px]">
+    <div className="flex justify-center px-4 md:px-6 lg:px-6">
+    <div className="flex justify-start  overflow-y-auto bg-gray-300  w-[400px] h-[400px]">
       <AllContact />
       
     </div>
